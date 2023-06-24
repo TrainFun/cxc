0f25312f0adfb05ee4ad9931df1d0a1a7fc692ee
TardyPurcell <39642409+TrainFun@users.noreply.github.com>
2023-06-25 06:51:27 +0800

    test: ...

--- test.sh
+++ test.sh
@@ -27,13 +27,24 @@ while [[ -e $code ]]; do
 		./bin/main $code > /tmp/cxcode 2>/dev/null
 	fi
 	input="test/$1_$i.in"
+	set +e;
 	if [[ -e $input ]] ; then
 		lli /tmp/cxcode < $input > /tmp/cxout
 	else
 		lli /tmp/cxcode > /tmp/cxout
 	fi
+	exitcode=$?
+	if [[ $verbose -ne 1 ]]; then
+		set -e;
+	fi
 	printf "\033[0m"
 	diff -y --suppress-common-lines --color=always /tmp/cxout "test/$1_$i.out"
+	if [[ -e "test/$1_$i.ret" ]]; then
+		echo '
+------------------------------------------------
+'
+		diff -y --suppress-common-lines --color=always <(echo $exitcode) "test/$1_$i.ret"
+	fi
 
 	i=$((i+1))
 	code="test/$1_$i.c"
