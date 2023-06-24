3c246295afdbfea8e77fc9416def83d51592bfa3
TardyPurcell <tardypurcell@outlook.com>
2023-06-25 06:25:06 +0800

    test: ...

---
+++ test/pass_15.c
@@ -0,0 +1,9 @@
+int main() {
+  write cast<double>(123);
+  write cast<double>(true);
+  write cast<int>(.);
+  write cast<int>(true) + 1145;
+  write cast<bool>(123);
+  write cast<bool>(124);
+  write cast<bool>(.);
+}
